988cb616314ddfdacac40f142a688d82e27b6b6a
finaljas90 <jferehawk@gmail.com>
2026-07-13 10:33:51 -0700

    Update resource-data.mjs

--- src/resources/resource-data.mjs
+++ src/resources/resource-data.mjs
@@ -1512,7 +1512,6 @@ const SCION = {
       description: "<strong>Heed the Call to Glory</strong> (Mythweaver, triggered action): The target ally gains a surge, the targets can then shift R ([[@characteristics.reason.value]]) squares. After the movement, or if they choose not to move, the targets gain an edge on their next ability used before the end of the encounter if they are adjacent to an enemy. <br><strong>Spend 1 Balance:</strong> The targets shift 2xR ([[2 * @characteristics.reason.value]]) instead.",
       cost: 1,
       minLevel: 1,
-      requiresSubclass: "Auteur",
   passiveEffects: [],
 };
 // ── Registry ─────────────────────────────────────────────────────────────────
